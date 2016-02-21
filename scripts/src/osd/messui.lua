4466a96a2ffaf5f72dfc1b3e2282cf6e2e853a59
Robbbert <pac0446@bigpond.net.au>
2016-02-21 21:46:18 +1100

    Fixed the build.

--- scripts/src/osd/messui.lua
+++ scripts/src/osd/messui.lua
@@ -149,6 +149,8 @@ project ("osd_" .. _OPTIONS["osd"])
 		MAME_DIR .. "src/osd/windows/input.cpp",
 		MAME_DIR .. "src/osd/windows/output.cpp",
 		MAME_DIR .. "src/osd/windows/video.cpp",
+		MAME_DIR .. "src/osd/modules/osdwindow.cpp",
+		MAME_DIR .. "src/osd/modules/osdwindow.h",
 		MAME_DIR .. "src/osd/windows/window.cpp",
 		MAME_DIR .. "src/osd/winui/newui.cpp",
 		MAME_DIR .. "src/osd/windows/winmain.cpp",
