e31b64568093a9443d055061442e2f240f7cdf29
Robbbert <pac0446@bigpond.net.au>
2016-04-28 10:13:17 +1000

    0.173 Release Patch

--- docs/release/build/newsrc.bat
+++ docs/release/build/newsrc.bat
@@ -4,13 +4,13 @@ set to=c:\MESS\docs\release
 rd %to%\src /q /s
 
 md %to%\src\emu
-copy /Y %from%\src\emu\audit.* %to%\src\emu
 copy /Y %from%\src\emu\emuopts.cpp %to%\src\emu
 copy /Y %from%\src\emu\video.* %to%\src\emu
 copy /Y %from%\src\version.cpp %to%\src
 
-md %to%\src\emu\ui
-copy /Y %from%\src\emu\ui\ui.cpp %to%\src\emu\ui
+md %to%\src\frontend\mame\ui
+copy /Y %from%\src\frontend\mame\audit.* %to%\src\frontend\mame
+copy /Y %from%\src\frontend\mame\ui\ui.cpp %to%\src\frontend\mame\ui
 
 md %to%\src\lib\util
 copy /Y %from%\src\lib\util\options.cpp %to%\src\lib\util
@@ -20,10 +20,6 @@ copy /Y %from%\src\osd\windows\winmain.cpp %to%\src\osd\windows
 
 md %to%\src\osd\winui
 xcopy /E /Y %from%\src\osd\winui %to%\src\osd\winui
-
-md %to%\src\mame
-md %to%\src\mame\osd
-xcopy /E /Y %from%\src\mame\osd %to%\src\mame\osd
 
 rem now save all our stuff to github
 copy %from%\*.bat %to%\build
@@ -35,7 +31,7 @@ type %from%\docs\floppy.txt      | MORE /P > %to%\docs\floppy.txt
 type %from%\docs\hlsl.txt        | MORE /P > %to%\docs\hlsl.txt
 type %from%\docs\imgtool.txt     | MORE /P > %to%\docs\imgtool.txt
 type %from%\docs\mame.txt        | MORE /P > %to%\docs\mame.txt
-type %from%\docs\mamelicense.txt | MORE /P > %to%\docs\mamelicense.txt
+type %from%\docs\LICENSE         | MORE /P > %to%\docs\license.txt
 type %from%\docs\newvideo.txt    | MORE /P > %to%\docs\newvideo.txt
 type %from%\docs\nscsi.txt       | MORE /P > %to%\docs\nscsi.txt
 type %from%\docs\SDL.txt         | MORE /P > %to%\docs\SDL.txt
