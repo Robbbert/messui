b60935771e6d29c1da0af975c35c0e841475c272
Robbbert <Robbbert@users.noreply.github.com>
2016-09-26 22:53:45 +1000

    fix the build. (but crashes if no machines in unavailable)

--- src/osd/winui/winui.cpp
+++ src/osd/winui/winui.cpp
@@ -4108,7 +4108,7 @@ static BOOL MameCommand(HWND hwnd,int id, HWND hwndCtl, UINT codeNotify)
 		InvalidateRect(hMain,NULL,TRUE);
 		break;
 
-		/*
+        /*
           Switches to fullscreen mode. No check mark handling
           for this item cause in fullscreen mode the menu won't
           be visible anyways.
@@ -4119,10 +4119,10 @@ static BOOL MameCommand(HWND hwnd,int id, HWND hwndCtl, UINT codeNotify)
 
 	case ID_TOOLBAR_EDIT:
 		{
-			char buf[256];
+			std::string buf;
 			HWND hToolbarEdit;
 
-			win_get_window_text_utf8(hwndCtl, buf, sizeof(buf));
+			buf = win_get_window_text_utf8(hwndCtl);
 			switch (codeNotify)
 			{
 			case TOOLBAR_EDIT_ACCELERATOR_PRESSED:
@@ -4131,23 +4131,23 @@ static BOOL MameCommand(HWND hwnd,int id, HWND hwndCtl, UINT codeNotify)
 				break;
 			case EN_CHANGE:
 				//put search routine here first, add a 200ms timer later.
-				if ((!_stricmp(buf, SEARCH_PROMPT) && !_stricmp(g_SearchText, "")) ||
-				    (!_stricmp(g_SearchText, SEARCH_PROMPT) && !_stricmp(buf, "")))
+				if ((!_stricmp(buf.c_str(), SEARCH_PROMPT) && !_stricmp(g_SearchText, "")) ||
+					(!_stricmp(g_SearchText, SEARCH_PROMPT) && !_stricmp(buf.c_str(), "")))
 				{
-					strcpy(g_SearchText, buf);
+					strcpy(g_SearchText, buf.c_str());
 				}
 				else
 				{
-					strcpy(g_SearchText, buf);
+					strcpy(g_SearchText, buf.c_str());
 					ResetListView();
 				}
 				break;
 			case EN_SETFOCUS:
-				if (!_stricmp(buf, SEARCH_PROMPT))
+				if (!_stricmp(buf.c_str(), SEARCH_PROMPT))
 					win_set_window_text_utf8(hwndCtl, "");
 				break;
 			case EN_KILLFOCUS:
-				if (*buf == 0)
+				if (*buf.c_str() == 0)
 					win_set_window_text_utf8(hwndCtl, SEARCH_PROMPT);
 				break;
 			}
