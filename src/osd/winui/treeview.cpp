8e7dbf3f71d6a87141b7f421ff096cca56f93305
Robbbert <Robbbert@users.noreply.github.com>
2017-08-26 00:54:38 +1000

    *Fixed bugs with imperfect games, and parents/clones

--- src/osd/winui/treeview.cpp
+++ src/osd/winui/treeview.cpp
@@ -1133,22 +1133,22 @@ void CreateDeficiencyFolders(int parent_index)
 
 	for (int jj = 0; jj < nGames; jj++)
 	{
-		if (driver_list::driver(jj).flags & MACHINE_WRONG_COLORS)
+		if (driver_list::driver(jj).type.unemulated_features() & device_t::feature::PALETTE)
 			AddGame(lpWrongCol,jj);
 
-		if (driver_list::driver(jj).flags & MACHINE_UNEMULATED_PROTECTION)
+		if (driver_list::driver(jj).type.unemulated_features() & device_t::feature::PROTECTION)
 			AddGame(lpProt,jj);
 
-		if (driver_list::driver(jj).flags & MACHINE_IMPERFECT_COLORS)
+		if (driver_list::driver(jj).type.imperfect_features() & device_t::feature::PALETTE)
 			AddGame(lpImpCol,jj);
 
-		if (driver_list::driver(jj).flags & MACHINE_IMPERFECT_GRAPHICS)
+		if (driver_list::driver(jj).type.imperfect_features() & device_t::feature::GRAPHICS)
 			AddGame(lpImpGraph,jj);
 
-		if (driver_list::driver(jj).flags & MACHINE_NO_SOUND)
+		if (driver_list::driver(jj).type.unemulated_features() & device_t::feature::SOUND)
 			AddGame(lpMissSnd,jj);
 
-		if (driver_list::driver(jj).flags & MACHINE_IMPERFECT_SOUND)
+		if (driver_list::driver(jj).type.imperfect_features() & device_t::feature::SOUND)
 			AddGame(lpImpSnd,jj);
 
 		if (driver_list::driver(jj).flags & MACHINE_NO_COCKTAIL)
