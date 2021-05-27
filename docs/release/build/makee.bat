b8c19f9ad58853a74ea1bbcd8ec1a67cc850995d
Robbbert <Robbbert@users.noreply.github.com>
2021-05-27 20:59:58 +1000

    0.232 release files

--- docs/release/build/makee.bat
+++ docs/release/build/makee.bat
@@ -3,6 +3,7 @@ del build\generated\resource\messvers.rc
 :start
 del messui.exe
 if exist messui.exe goto start
+del messui.sym
 copy /Y src\mame\mess.flt src\mame\mess.bak
 copy /Y src\mame\messui.txt src\mame\mess.flt
 touch src\mame\mess.flt
@@ -12,5 +13,4 @@ touch src\osd\winui\mameui.rc
 call make64 -j6 "OSD=messui" %1 %2 %3
 copy /Y src\mame\mess.bak src\mame\mess.flt
 if not exist messui.exe goto end
-del messui.sym
 :end
